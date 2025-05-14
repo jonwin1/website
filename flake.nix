f69e8ec431c4ed1bd00fe95e1b1abf9499d05d01
Jonatan Wincent <jonatan.wincent@gmail.com>
2025-05-14 20:31:21 +0200

    add .envrc

--- flake.nix
+++ flake.nix
@@ -1,27 +1,33 @@
 {
-  description = "Slstatus flake by jonwin";
+  description = "Hugo flake";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          hugo
-          go
-          dart-sass
-        ];
-
-        shellHook = ''
-          exec zsh
-        '';
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            hugo
+            go
+            dart-sass
+          ];
+        };
+      }
+    );
 }
